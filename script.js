fac37af4af987c58bf830ec08f4838df5615b70d
SHAIK MAHAMMAD ALI <shaikmahammad1319@gmail.com>
2025-12-09 12:13:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,7 @@
-const correctPassword = "roshini";  // <-- Change here
-
-function checkPassword() {
-    const input = document.getElementById("password-input").value;
-    const error = document.getElementById("error-msg");
-    const box = document.querySelector(".password-box");
-
-    if (input === correctPassword) {
-        error.style.color = "#00ff99";
-        error.innerText = "Unlocked 💖";
-
-        setTimeout(() => {
-            document.getElementById("password-screen").style.display = "none";
-        }, 600);
-
-    } else {
-        error.style.color = "#ff3333";
-        error.innerText = "Wrong password ❌";
-        box.classList.add("shake");
-
-        setTimeout(() => {
-            box.classList.remove("shake");
-        }, 500);
-    }
-}
-
 // =========================
 // CHANGE PASSWORD HERE 👇
 // =========================
-const SECRET_PASSWORD = "1234";   // ← change your password here
+const SECRET_PASSWORD = "1319";   // ← change your password here
 
 
 // PASSWORD CHECK
